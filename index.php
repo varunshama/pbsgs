6008ef7fe5a071d1656d8505a4be17414a0c5e0a
varunshama <47223715+varunshama@users.noreply.github.com>
2019-02-03 22:19:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -221,7 +221,7 @@
                                           </div>
 
                                           <div class="item">
-                                            <img src="content/ngo/images/slider/website.png" alt="Chicago" style="width:100%;">
+                                            <img src="content/ngo/images/slide,r/website.png" alt="Chicago" style="width:100%;">
                                           </div>
                                             
                                             <div class="item">
@@ -242,12 +242,13 @@
                                     </div>               
                                                                 <div class="tp-caption   tp-resizeme" id="slide-1-layer-2" data-x="60" data-y="bottom" data-voffset="" data-width="['none','none','none','none']" data-height="['none','none','none','none']" data-transform_idle="o:1;" data-transform_in="opacity:0;s:800;e:Power2.easeInOut;" data-transform_out="opacity:0;s:300;s:300;" data-start="700" data-responsive_offset="on" style="z-index: 6;"><img src="content/ngo/images/bg.png" alt="" width="352" height="327" data-ww="352px" data-hh="327px" data-no-retina> </div>
                                                                 <div class="tp-caption mfnrs_ngo_medium_dark   tp-resizeme" id="slide-1-layer-3" data-x="110" data-y="bottom" data-voffset="30" data-width="['auto']" data-height="['auto']" data-transform_idle="o:1;" data-transform_in="y:-50px;opacity:0;s:800;e:Power2.easeInOut;" data-transform_out="opacity:0;s:300;s:300;" data-start="1100" data-splitin="none" data-splitout="none" data-responsive_offset="on" style="z-index: 7; white-space: nowrap; color: #fff !important; position:relative; right: 30px; text-align:left ">
+                                                                    THANK YOU<br> 
+                                                                    Everyone<br>
+                                                                    For Making<br>
                                                                     41st varshik<br>
                                                                     sneha samelan<br>
-                                                                    Sunday 20th<br> 
-                                                                    january 2019<br>
-                                                                    Thathai bhatia hall,<br>
-                                                                    Kandivali West<br>
+                                                                    Successful<br>
+                                                                    
                                                                 </div>
                                                             </li>
                                                             
